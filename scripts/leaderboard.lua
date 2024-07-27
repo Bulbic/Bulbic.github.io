18a1e5efc33a6d84d7a74eae2e0d14282d6bbfae
sunny-mc <168322204+sunny-mc@users.noreply.github.com>
2024-07-27 03:04:46 -0700

    Create leaderboard.lua

---
+++ scripts/leaderboard.lua
@@ -0,0 +1,6 @@
+local nameOfItem = "Money" -- Change this to the name of the leaderboard stat (ex: Cash, Points, Money, Kills, etc)
+local valueToPut = 50 -- Value you want it to be at
+local lp = game.Players.LocalPlayer
+
+-- DON'T change the nameOfValue or valueToPur --
+lp.leaderstats[nameOfValue].Value = valueToPut
